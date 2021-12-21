5d7b4e39342d56d9abc75337f2c16ac1325dce21
myanir <myanir@gmail.com>
2021-12-21 14:24:52 +0200

    534fix

--- 534.py
+++ 534.py
@@ -1,11 +1,12 @@
 #function check if the last charcter avilable beofore in the  string
 def last_early(my_str):
     last=my_str[-1]
-    if last in my_str[1:-2]:
+    if last in my_str[:-1]:
+        print("ok")
+    else:
+        print("False")
         return True
 str_to_chk=input("please enter a string to check if the last charcter showed before: ")
-ok=last_early (str_to_chk)
-if ok ==True:
-    print(ok)
-else:
-    print ("False")
+str_to_chk=str_to_chk.lower()
+last_early (str_to_chk)
+
